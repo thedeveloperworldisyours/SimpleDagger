36286ea5132de92c8c954fc89dd117d6bdd007ee
Cabezas <javiergonzalezcabezas@gmail.com>
2019-04-28 18:42:56 +0100

    field injection

--- app/src/main/java/com/thedeveloperworldisyours/simpledagger/CarComponent.kt
+++ app/src/main/java/com/thedeveloperworldisyours/simpledagger/CarComponent.kt
@@ -6,4 +6,6 @@ import dagger.Component
 interface CarComponent {
 
     var car:Car
+
+    fun inject(mainActivity: MainActivity)
 }
--- app/src/main/java/com/thedeveloperworldisyours/simpledagger/MainActivity.kt
+++ app/src/main/java/com/thedeveloperworldisyours/simpledagger/MainActivity.kt
@@ -3,18 +3,22 @@ package com.thedeveloperworldisyours.simpledagger
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var car :Car
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var carComponent:CarComponent = DaggerCarComponent.create()
+        val carComponent:CarComponent = DaggerCarComponent.create()
 
+        carComponent.inject(this)
 
-        text_view.text = carComponent.car.drive()
+        text_view.text = car.drive()
 
     }
 }
